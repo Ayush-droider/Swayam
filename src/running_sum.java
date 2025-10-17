097e11330347e56bc9eda548a267aeef1fc0b87a
Ayush Pandey <ayush.pandey9605@gmail.com>
2025-10-17 20:39:31 +0530

    Initial commit

---
+++ src/running_sum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class running_sum {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter Array size--->");
+        int n=in.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        int[] ans = new int[arr.length];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            ans[i] = sum;
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
