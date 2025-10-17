cc454b4f18fb107320ef76dbf3f64b0bfddac9eb
Ayush Pandey <ayush.pandey9605@gmail.com>
2025-10-17 15:49:10 +0530

    Initial commit

---
+++ src/array_reverse.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class array_reverse {
+    public static void swap(int[] arr,int start,int end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+    }
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter Array size--->");
+        int n=in.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        int start=0,end=n-1;
+        while(start<end){
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
