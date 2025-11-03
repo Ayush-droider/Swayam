3392c6c2d981194183df5545f8e80b9a85ff9cac
Ayush Pandey <pandey.ayush9605@gmail.com>
2025-11-03 20:50:18 +0530

    Initial commit

---
+++ src/LinkedList/midLinkedList.java
@@ -0,0 +1,67 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class midLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node head;
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) temp = temp.next;
+        temp.next = newNode;
+    }
+
+    public void findMiddle() {
+        if (head == null) {
+            System.out.println("List is empty!");
+            return;
+        }
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println("Middle element: " + slow.data);
+    }
+
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty!");
+            return;
+        }
+        Node temp = head;
+        System.out.print("Linked List: ");
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        midLinkedList list = new midLinkedList();
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) list.insertAtEnd(sc.nextInt());
+
+        list.display();
+        list.findMiddle();
+    }
+}
