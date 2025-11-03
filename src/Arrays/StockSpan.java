605d279686e94e3e0e8938589dc314902ecfaac6
Ayush Pandey <pandey.ayush9605@gmail.com>
2025-11-03 21:15:30 +0530

    Initial commit

---
+++ src/Arrays/StockSpan.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.*;
+
+public class StockSpan {
+
+    public static int[] calculateSpan(int[] prices) {
+        int n = prices.length;
+        int[] span = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && prices[st.peek()] <= prices[i])
+                st.pop();
+            span[i] = st.isEmpty() ? (i + 1) : (i - st.peek());
+            st.push(i);
+        }
+        return span;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of days: ");
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+        System.out.println("Enter stock prices:");
+        for (int i = 0; i < n; i++) prices[i] = sc.nextInt();
+
+        int[] span = calculateSpan(prices);
+
+        System.out.println("Stock Span for each day:");
+        for (int i = 0; i < n; i++)
+            System.out.println(prices[i] + " -> " + span[i]);
+    }
+}
