6c9196c35b5551d94c9ad8e2432662e88c4ae62a
Ayush Pandey <pandey.ayush9605@gmail.com>
2025-11-04 21:40:50 +0530

    Initial Commit

---
+++ src/String/Validana.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.util.*;
+
+public class Validana {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) freq[c - 'a']++;
+        for (char c : t.toCharArray()) freq[c - 'a']--;
+        for (int f : freq) if (f != 0) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Validana obj = new Validana();
+        System.out.print("Enter first string: ");
+        String s = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String t = sc.nextLine();
+        System.out.println("Is anagram: " + obj.isAnagram(s, t));
+    }
+}
+
