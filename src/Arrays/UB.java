88f644d848408140659393fcac00ebb953ae3f39
Ayush Pandey <pandey.ayush9605@gmail.com>
2025-11-04 20:45:30 +0530

    Initial commit

---
+++ src/Arrays/UB.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.util.*;
+
+public class UB {
+    public static int upperBound(int[] arr, int x) {
+        int low = 0, high = arr.length;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= x)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        int x = sc.nextInt();
+        int index = upperBound(arr, x);
+        if (index == n)
+            System.out.println("Upper bound not found");
+        else
+            System.out.println(index);
+    }
+}
+
