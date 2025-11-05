9463c872eab35f0c251d32f90ac6a21ef2128e44
Ayush Pandey <pandey.ayush9605@gmail.com>
2025-11-05 19:40:15 +0530

    Initial commit

---
+++ src/PriorityQueue/KthLargest.java
@@ -0,0 +1,20 @@
+package PriorityQueue;
+
+import java.util.*;
+
+public class KthLargest {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        System.out.println(findKthLargest(arr, k));
+    }
+}
