140e92b7e9256d13e880c44074bb141ff95f2267
Ayush Pandey <pandey.ayush9605@gmail.com>
2025-11-05 19:51:22 +0530

    Initial commit

---
+++ src/PriorityQueue/SlidingWindowMaximum.java
@@ -0,0 +1,27 @@
+package PriorityQueue;
+
+import java.util.*;
+
+public class SlidingWindowMaximum {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0) return new int[0];
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) dq.pollFirst();
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast();
+            dq.addLast(i);
+            if (i >= k - 1) res[i - k + 1] = nums[dq.peekFirst()];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        int[] res = maxSlidingWindow(nums, k);
+        for (int x : res) System.out.print(x + " ");
+    }
+}
+
