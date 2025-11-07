a61e8efa59b2bead108ff14cfabfe47d4ee3883c
Ayush Pandey <pandey.ayush9605@gmail.com>
2025-11-07 23:24:12 +0530

    Initial commit

---
+++ src/Arrays/LemonadeChange.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class LemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int b : bills) {
+            if (b == 5) five++;
+            else if (b == 10) {
+                if (five == 0) return false;
+                five--; ten++;
+            } else {
+                if (ten > 0 && five > 0) {
+                    ten--; five--;
+                } else if (five >= 3) five -= 3;
+                else return false;
+            }
+        }
+        return true;
+    }
+}
