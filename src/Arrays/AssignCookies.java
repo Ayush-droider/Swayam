c65cd46d9a7e8fe67f2b38aa931ba3c227059dd8
Ayush Pandey <pandey.ayush9605@gmail.com>
2025-11-07 23:21:55 +0530

    Initial commit

---
+++ src/Arrays/AssignCookies.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.*;
+public class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0, j = 0;
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) i++;
+            j++;
+        }
+        return i;
+    }
+}
+
