913806b2badbba24aebcaeee5716ae9f07aa50b4
Ayush Pandey <pandey.ayush9605@gmail.com>
2025-11-08 00:44:00 +0530

    Initial commit

---
+++ src/DynamicProgramming/CoinChange2.java
@@ -0,0 +1,18 @@
+package DynamicProgramming;
+
+public class CoinChange2 {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins)
+            for (int i = coin; i <= amount; i++)
+                dp[i] += dp[i - coin];
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        CoinChange2 c = new CoinChange2();
+        int[] coins = {1, 2, 5};
+        System.out.println(c.change(5, coins));
+    }
+}
