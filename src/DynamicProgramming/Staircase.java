55b3882072aa25b508e668875b3222e1cab73f47
Ayush Pandey <pandey.ayush9605@gmail.com>
2025-11-08 00:19:56 +0530

    Initial commit

---
+++ src/DynamicProgramming/Staircase.java
@@ -0,0 +1,19 @@
+package DynamicProgramming;
+
+public class Staircase {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+        int a = 1, b = 2;
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        Staircase s = new Staircase();
+        System.out.println(s.climbStairs(5));
+    }
+}
